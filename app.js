c26620df52c04b8af1e7651d2498ca80ec7fd1a7
Nicholas Romolo <134345990+norskrom6@users.noreply.github.com>
2023-06-11 19:29:39 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const db = require('./db');
+
+app.use('/', routes);
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
